41cd240f31730b27ea3d7a6ee793ef2b6bae8d11
alejo760 <62118830+alejo760@users.noreply.github.com>
2022-12-05 10:21:59 -0500

    ...

--- main.py
+++ main.py
@@ -36,7 +36,7 @@ def generate_qr_codes(df):
     qr = pyqrcode.create(url)
     # Save the QR code as an image file
     #append to qrdict
-    qr_png.update({row['id']:qr.png_as_base64_str('qr.png', scale=6)})
+    qr_png.update({row['id']:qr.png_as_base64_str( scale=6)})
   return df["qr"].qr_png
 
 
