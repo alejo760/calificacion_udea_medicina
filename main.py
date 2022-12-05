e83d47d15f6d976316ae4c371b3cf873696c3212
alejo760 <62118830+alejo760@users.noreply.github.com>
2022-12-05 17:02:35 -0500

    ....

--- main.py
+++ main.py
@@ -15,6 +15,7 @@ from google.oauth2 import service_account
 
 @st.experimental_singleton
 def get_db():
+    global db
     key_dict = json.loads(st.secrets["textkey"])
     creds=service_account.Credentials.from_service_account_info(key_dict)
     db = firestore.Client(credentials=creds, project="estudiantesudea-1bbcd")
