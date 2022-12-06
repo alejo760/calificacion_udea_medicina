d0eeb1f738e03ff6dcf22817367dd7b8f74254c9
alejo760 <62118830+alejo760@users.noreply.github.com>
2022-12-05 19:43:34 -0500

    .......

--- main.py
+++ main.py
@@ -29,7 +29,7 @@ def generate_qr_codes(df):
   for i, row in df.iterrows():
     # Generate the QR code
     url = pyqrcode.create(row['id'])
-    url.png(f"{row['id']} {row['name']}.png", scale=6)
+    url.png_as_base64_str(f"{row['id']} {row['name']}.png", scale=6)
 
     # Download the QR code
     image = open(f"{row['id']}.png", "rb")
