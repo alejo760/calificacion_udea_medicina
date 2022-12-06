cbac941aefb5a81b143cb2a4b9cffa87b4b9c3f0
alejo760 <62118830+alejo760@users.noreply.github.com>
2022-12-05 19:19:13 -0500

    ....

--- main.py
+++ main.py
@@ -12,8 +12,12 @@ from firebase_admin import firestore
 
 key_dict = json.loads(st.secrets["textkey"])
 creds=credentials.Certificate(key_dict)
-app = firebase_admin.initialize_app(creds)
-db = firebase_admin.get_app(credentials=creds, project="estudiantesudea-1bbcd")
+
+# Provide a unique name for the app
+app = firebase_admin.initialize_app(creds, name="calificacionudea")
+
+# Use the app name to get the app instance
+db = firebase_admin.get_app(credentials=creds, project="estudiantesudea-1bbcd", name="calificacionudea")
 
 # Function to upload a database in xlsx format with the list of students name, e-mail, and id
 def upload_database():
