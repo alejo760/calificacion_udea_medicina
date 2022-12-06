66c140bf128894b0abedf014a5ac61758491a4e0
alejo760 <62118830+alejo760@users.noreply.github.com>
2022-12-06 08:40:21 -0500

    .....

--- main.py
+++ main.py
@@ -19,22 +19,25 @@ db = firestore.Client(credentials=creds, project="estudiantesudea-1bbcd")
 
 # Create a calification page that shows the student info and a form that allows the teacher to calificate the student from 0.0 to 5.0
 def calification_page(student_id):
-  student_ref = db.collection("students").document(student_id)
-  student = student_ref.get().to_dict()
-  st.write(f"Name: {student['name']}")
-  st.write(f"E-mail: {student['email']}")
-  score = st.slider("Enter calification (0.0-5.0):", min_value=0.0, max_value=5.0, step=0.1)
-  # Store the calification in Firestore
-  student_ref.update({'calification': score})
+  try:
+    student_ref = db.collection("students").document(student_id)
+    student = student_ref.get().to_dict()
+    st.write(f"Name: {student['name']}")
+    st.write(f"E-mail: {student['email']}")
+    score = st.slider("Calificar el estdiente (0.0 - 5.0):", min_value=0.0, max_value=5.0, step=0.1)
+    # Store the calification in Firestore
+    student_ref.update({'calification': score})
+  except:
+    st.write("No se encontró el estudiante")
 
 
 # Main function
 def main():
-  st.title("Calification App")
+  st.title("App de calificacion Medicina UdeA prueba hecha por Alejandro Hernández-Arango MD")
   # Calification page
   student_id = st.experimental_get_query_params().get("student_id")
   if student_id is None:
-      student_id = st.text_input("Enter student id:")
+      student_id = st.text_input("Escriba la CC del estudiante:")
       calification_page(student_id)
   else:
       calification_page(student_id[0])
