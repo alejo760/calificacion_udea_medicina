2c2aac6914159733721dbfc23c0a191fcc7c1b59
alejo760 <62118830+alejo760@users.noreply.github.com>
2022-12-07 16:53:17 -0500

    ....

--- main.py
+++ main.py
@@ -74,7 +74,7 @@ def calification_page(student_id):
 
 
               except:
-                st.warning("No se encontró el estudiante o el codigo QR no fue leido profavor comunicarse con: \n Alejandro.Hernandeza@udea.edu.co")
+                st.warning("No se encontró el estudiante o el codigo QR no fue leido porfavor comunicarse con: \n Alejandro.Hernandeza@udea.edu.co")
             else:
                 st.warning('Login fallido, revise las credenciales de acceso son las mismas del Ghips')
 
