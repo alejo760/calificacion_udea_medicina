245ec097149a47616e96df6c61b5b6a110ca1bd7
alejo760 <62118830+alejo760@users.noreply.github.com>
2022-12-07 18:10:15 -0500

    ....

--- main.py
+++ main.py
@@ -83,7 +83,10 @@ def calification_page(student_id):
 
 # Main function
 def main():
-  st.image("https://portal.udea.edu.co/wps/wcm/connect/udea/bb031677-32be-43d2-8866-c99378f98aeb/1/Logo+Facultad+color+%282%29.png?MOD=AJPERES")
+  # Set the page layout
+  st.set_page_config(page_title="Calificación VIII Medicina Interna UdeA", page_icon=":bar_chart:", layout="wide", initial_sidebar_state="expanded")
+
+  st.image("https://portal.udea.edu.co/wps/wcm/connect/udea/bb031677-32be-43d2-8866-c99378f98aeb/1/Logo+Facultad+color+%282%29.png?MOD=AJPERES", width=200)
   st.title("App de calificacion VIII Medicina Interna UdeA")
   st.write("prueba hecha por Alejandro Hernández-Arango MD")
   # Calification page
