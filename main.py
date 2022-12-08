926eb834b7c9dc8ccb157108a9f4eb0776029972
alejo760 <62118830+alejo760@users.noreply.github.com>
2022-12-08 14:28:13 -0500

    ............

--- main.py
+++ main.py
@@ -70,7 +70,7 @@ def main():
   st.image("https://portal.udea.edu.co/wps/wcm/connect/udea/bb031677-32be-43d2-8866-c99378f98aeb/1/Logo+Facultad+color+%282%29.png?MOD=AJPERES", width=200)
   st.title("App de calificacion VIII Medicina Interna UdeA")
   st.write("hecha por Alejandro Hernández-Arango internista MD")
-  loginexitoso= False
+  loginexitoso =False
   student_id = st.experimental_get_query_params().get("student_id")
   with st.sidebar:
     usuario= st.text_input('Usuario')
