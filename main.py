079c48540d2ecf9f7bca177131b042bea3d9873d
alejo760 <62118830+alejo760@users.noreply.github.com>
2022-12-08 11:02:12 -0500

    -.......

--- main.py
+++ main.py
@@ -19,11 +19,8 @@ creds = service_account.Credentials.from_service_account_info(key_dict)
 db = firestore.Client(credentials=creds, project="estudiantesudea-1bbcd")
 
 
-
-# Create a calification page that shows the student info and a form that allows the teacher to calificate the student from 0.0 to 5.0
-def calification_page(student_id):
-
-  with st.form(key='login'):
+def login(loginexitoso):
+    with st.form(key='login'):
         usuario= st.text_input('Usuario')
         clave= st.text_input('Clave',type="password")
         st.form_submit_button('Login')
@@ -33,11 +30,18 @@ def calification_page(student_id):
             x = requests.post(url, data = password)
             response_status = x.status_code
             if response_status == 200 or usuario=='roben1319@yahoo.com' or usuario=='dandres.velez@udea.edu.co':
-              if student_id is None:
+              loginexitoso= True
+            else:
+                st.warning('Login fallido, revise las credenciales de acceso son las mismas del Ghips')
+    return loginexitoso, usuario
+# Create a calification page that shows the student info and a form that allows the teacher to calificate the student from 0.0 to 5.0
+def calification_page(student_id, usuario):
+  
+  if student_id is None:
                 st.warning("el codigo QR no fue leido adecuadamente:")
-              else:
-                pass
-              try:
+  else:
+
+    try:
                   student_ref = db.collection("students").document(student_id)
                   student = student_ref.get().to_dict()
                   st.write(f"Nombre: {student['name']}")
@@ -47,11 +51,9 @@ def calification_page(student_id):
                   if numero_calificaciones == 4:
                     st.write("El estudiante ya tiene 4 calificaciones, no se puede calificar")
                   else:
-                    with st.form(key='calificar'):
+                    if st.button("Calificar"):
                         score = st.slider("Calificar el estdiente (0.0 - 5.0):", min_value=0.0, max_value=5.0, step=0.1,)
                         concepto= st.text_area('escriba un concepto sobre el estudiante')
-                        numero_calificaciones=student.get("calificaciones") 
-                        st.form_submit_button("Calificar")
                     if numero_calificaciones == 0:
                         student_ref.update({"profesor": usuario})
                         student_ref.update({"calificacion": score})
@@ -77,15 +79,9 @@ def calification_page(student_id):
                         student_ref.update({"calificaciones": numero_calificaciones+1})
                         st.success("Estudiante calificado y nota guardada exitosamente")
 
-              except Exception as e:
+    except Exception as e:
                 st.warning("Error en la calificacion porfavor comunicarse con: \n Alejandro.Hernandeza@udea.edu.co")
-                st.warning(e)             
-
-            #else:
-                #st.warning('Login fallido, revise las credenciales de acceso son las mismas del Ghips')
-
-
-
+                st.warning(e)                   
 # Main function
 def main():
   # Set the page layout
@@ -93,15 +89,15 @@ def main():
   st.image("https://portal.udea.edu.co/wps/wcm/connect/udea/bb031677-32be-43d2-8866-c99378f98aeb/1/Logo+Facultad+color+%282%29.png?MOD=AJPERES", width=200)
   st.title("App de calificacion VIII Medicina Interna UdeA")
   st.write("hecha por Alejandro Hernández-Arango internista MD")
-  # Calification page
+  loginexitoso= False
   student_id = st.experimental_get_query_params().get("student_id")
-  if student_id is None:
-      calification_page(student_id)
+  if login(loginexitoso)==True:
+    calification_page(student_id)
   else:
-      calification_page(student_id[0])
+    pass
 
 
-# Run the appsjadnkajsdn
+# Run the main function
 if __name__ == "__main__":
   main()
 
