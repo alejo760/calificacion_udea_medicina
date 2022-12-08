e8528d46e1b3edccb5dd8732b8237f0153524265
alejo760 <62118830+alejo760@users.noreply.github.com>
2022-12-08 14:53:09 -0500

    ....

--- main.py
+++ main.py
@@ -62,6 +62,7 @@ def main():
   student_id = st.experimental_get_query_params().get("student_id")
   student_ref = db.collection("students").document(str(student_id))
   student = student_ref.get().to_dict()
+  st.write(student)
   loginexitoso =0
   usuario= st.text_input('Usuario')
   clave= st.text_input('Clave',type="password")
