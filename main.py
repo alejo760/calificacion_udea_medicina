ccbf8f31905df3a69cf1b2862fc6332d70bc0f8c
alejo760 <62118830+alejo760@users.noreply.github.com>
2022-12-08 17:23:57 -0500

    ......

--- main.py
+++ main.py
@@ -52,7 +52,7 @@ def main():
     #mostrar las calificaciones anteriores
     if st.button('Mostrar calificaciones anteriores'):
       try:
-       df_student= pd.DataFrame(student, index=[0])
+       df_student= pd.DataFrame(student, index=["calificaciones"])
        st.table(df_student)
       except Exception as e:
         st.write(e)
