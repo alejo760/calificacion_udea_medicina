a5316c60147d14529fc41c0533a0ce5b394425e8
alejo760 <62118830+alejo760@users.noreply.github.com>
2023-03-16 14:39:13 -0500

    .......

--- main1.py
+++ main1.py
@@ -66,7 +66,7 @@ def generate_qr_codes(df, materia):
   return qr_png
 
 # Create a function to store all the data in Firestore and check if the student exists and if exists dont update
-def store_data_in_firestore():
+def store_data_in_firestore(df):
   for i, row in df.iterrows():
     student_ref = db.collection('students').document(str(int(row['id'])))
     student = student_ref.get()
@@ -113,7 +113,7 @@ def main():
     if df is not None:
       #set_time()
       fecha = set_time()
-      store_data_in_firestore()
+      store_data_in_firestore(df)
       st.success("Base de datos cargada exitosamente y guardada exitosamente")
       # Generate QR codes
       if st.button("Generar códigos QR"):
