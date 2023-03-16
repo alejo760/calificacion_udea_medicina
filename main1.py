3de8efc4154e810a1fb1b51c5da037aca8243cc1
alejo760 <62118830+alejo760@users.noreply.github.com>
2023-03-16 13:08:27 -0500

    .....

--- main1.py
+++ main1.py
@@ -116,7 +116,7 @@ def main():
     if df is not None:
       #set_time()
       fecha = set_time()
-      store_data_in_firestore(df, fecha, materia,collection)
+      store_data_in_firestore(df, materia,collection)
       st.success("Base de datos cargada exitosamente y guardada exitosamente")
       # Generate QR codes
       if st.button("Generar códigos QR"):
